bcabab667486964ee70567d3cb3fccf1fb9c38e0
Topgyal Gurung <topgyaltsering3@gmail.com>
2024-12-09 17:26:46 -0500

    markov

--- node-markov/makeText.js
+++ node-markov/makeText.js
@@ -1,2 +1,18 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require("fs");
+const MarkovMachine = require("./markov");
+const { type } = require("os");
+
+let text = fs.readFileSync("eggs.txt", { encoding: "utf8", flag: "r" });
+let mm = new MarkovMachine(text);
+
+// test1: output should be string test
+console.log(typeof mm.makeText(10) === "string");
+
+// test2" output length should not exceed numWords
+console.log(mm.makeText(10).split(" ").length <= 10);
+
+// test3: handle empty text input
+let emptyMm = new MarkovMachine("");
+console.log(emptyMm.makeText(10) === "");
--- node-markov/markov.js
+++ node-markov/markov.js
@@ -1,13 +1,11 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -16,14 +14,64 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
+  // build a map of chains of word -> possible next words
   makeChains() {
     // TODO
-  }
+    this.chains = {};
+    for (let i = 0; i < this.words.length; i++) {
+      const word = this.words[i];
+      const nextWord = this.words[i + 1] || null;
 
+      // if the word is already in the object, push nextword to its array
+      if (this.chains[word]) {
+        this.chains[word].push(nextWord);
+      } else {
+        // create a new array with an array containing next word
+        this.chains[word] = [nextWord];
+      }
+    }
+  }
 
   /** return random text from chains */
-
   makeText(numWords = 100) {
     // TODO
+    // pick a starting word randomly
+    // step1 find all words that can come after that word, pick one of those
+    // if picked null, stop
+    // otherwise restart at step 1
+    //e.g  "the cat in the hat"
+
+    // improvements to be made:
+    /** - have machine start on a capitalized word
+     * - stop at a period
+     * - bigrams - two words at a time
+     */
+
+    let keys = Object.keys(this.chains); //["the", "cat", "in", "hat"]
+    let startKey = keys[Math.floor(Math.random() * keys.length)]; // e.g "the"
+    const result = [];
+    // "the": ["cat", "hat"]
+    while (result.length < numWords && startKey != null) {
+      result.push(startKey); // result =["the"] /* next iter: ["the", "hat"]
+
+      const nextWords = this.chains[startKey]; // ["cat", "hat"] // null
+      startKey = nextWords[Math.floor(Math.random() * nextWords.length)]; //e.g "hat"
+    }
+    return result.join(" ");
   }
+  /**
+   * give URL, returns HTML mixed into the output text
+   * find libray in NPM that strip out HTML and use it
+   * user passing multiple files and or URLs make a machine that mixes together that text
+   */
 }
+
+let mm = new MarkovMachine("the cat in the hat");
+console.log(mm.makeText());
+console.log(mm.makeText((numWords = 50)));
+// example output:
+/* in the hat
+cat in the hat
+*/
+
+module.exports = MarkovMachine;
